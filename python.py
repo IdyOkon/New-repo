1533e3d5582ed8c7f5cc1720c97735cbd2de2fc7
idorenyin okon <okonidorenyin73@gmail.com>
2025-11-12 13:10:27 +0100

    created python.py

--- python.py
+++ python.py
@@ -1,1 +1,17 @@
 print(len('GITHUB tutorial'))
+
+
+def access_to_club(age):
+    if age > 17:
+        return ('enter') 
+    elif age == 0:
+        return ('access')
+    else: 
+       return('no access') 
+
+
+user_input = input('hey user enter any age for access grant\n')
+user_input_number = int(user_input)
+
+age = access_to_club(user_input_number)
+print(age)
